e4a75fd09b1d804642bca3f3aa5b5e613a35d26e
Injo Baik <baikinjo.28@gmail.com>
2017-04-02 21:57:21 -0700

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 
 var Examples = (props) => {
 	return(
-      <h3>Examples Component Modified!</h3>
+      <div>
+      	<h3>Examples Component Modified!</h3>
+      	<p>Welcome to examples page!</p>
+      </div>
     );
 }
 
