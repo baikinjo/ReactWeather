198d62ac08b0e8f1505dfe4f84a2e638e0fd9e9e
Injo Baik <baikinjo.28@gmail.com>
2017-04-03 21:05:17 -0700

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,34 @@ var About = React.createClass({
 */
 var About = (props) => {
 	return(
-      <h3>About Component</h3>
+		<div>
+			<h1 className="text-center">About</h1>
+			<p>
+				This is a weather application built on React. 
+			</p>
+			<p>
+				Here are some of the tools I used:
+			</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a> - Open Weather Map api to search for weather data.
+				</li>
+				<li>
+					<a href="http://www.heroku.com">Heroku</a> - Deploying the live server.
+				</li>
+			</ul>
+			<p>
+				Here is the <a href="https://github.com/baikinjo/ReactWeather">Source Code</a>
+			</p>
+			<p>
+				Check my <a href="http://injobaik.ca">Portfolio</a> site!
+			</p>
+
+		</div>
+      
     )
 };
 
